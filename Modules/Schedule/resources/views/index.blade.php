51252202ad2da9e2040a46c70ab632cac1223980
puntodamar <punto.damar2@gmail.com>
2025-09-04 16:34:37 +0700

    add: favicon

--- Modules/Schedule/resources/views/index.blade.php
+++ Modules/Schedule/resources/views/index.blade.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     @vite('resources/js/app.js') {{-- keep the global entry --}}
+{{--    <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" sizes="any">--}}
+{{--    <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/svg+xml">--}}
+{{--    <link rel="apple-touch-icon" href="{{ asset('assets/images/favicon.ico') }}">--}}
+
+    <link rel="icon" type="image/png" href="{{asset('favicon-96x96.png')}}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{asset('favicon.svg')}}" />
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png')}}" />
+    <link rel="manifest" href="{{asset('site.webmanifest')}}" />
     @inertiaHead
 </head>
 <body class="antialiased">
-@inertia
+    @inertia
 </body>
 </html>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,10 +23,11 @@
     </style>
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="{{asset('favicon-96x96.png')}}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{asset('favicon.svg')}}" />
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png')}}" />
+    <link rel="manifest" href="{{asset('site.webmanifest')}}" />
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
