b790a88d6f02ea230a51a671f849fa3f2d76332c
puntodamar <punto.damar2@gmail.com>
2025-09-09 16:13:52 +0700

    refactor: booking component

---
+++ resources/js/components/UI/booking/MembershipType.vue
@@ -0,0 +1,44 @@
+<template>
+    <h4 class="text-electric-magenta text-sm font-semibold mt-5">Jenis Paket</h4>
+    <fieldset>
+        <div class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+            <label
+                v-for="m in membership"
+                :key="m.id"
+                :aria-label="m.title"
+                class="group relative flex rounded-lg border border-gray bg-white p-4
+                 has-checked:outline-2 has-checked:-outline-offset-2 has-checked:outline-electric-magenta
+                 has-focus-visible:outline-3 has-focus-visible:-outline-offset-1
+                 has-disabled:border-gray has-disabled:bg-gray has-disabled:opacity-25
+                 dark:border-navy dark:bg-gray-900 dark:has-checked:bg-indigo-500/10"
+            >
+                <input
+                    type="radio"
+                    name="membership"
+                    :value="m.id"
+                    v-model="selectedMembershipId"
+                    class="absolute inset-0 appearance-none focus:outline-none cursor-pointer"
+                />
+                <div class="flex-1">
+                    <span class="block text-sm font-medium text-gray-800 dark:text-white">{{ m.title }}</span>
+                    <span v-if="m.description" class="mt-1 block text-xs text-gray-500 dark:text-gray-400">{{ m.description }}</span>
+                </div>
+                <CheckCircleIcon
+                    class="invisible size-5 text-electric-magenta group-has-checked:visible dark:text-electric-magenta"
+                    aria-hidden="true"
+                />
+            </label>
+        </div>
+    </fieldset>
+</template>
+
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/20/solid';
+import { ref } from 'vue';
+const membership = [
+    { id: 'insidental', title: 'Insidental', description: 'Sekali main' },
+    { id: 'member', title: 'Member', description: 'Harga lebih murah' },
+]
+const selectedMembershipId = ref(membership[0].id)
+
+</script>
