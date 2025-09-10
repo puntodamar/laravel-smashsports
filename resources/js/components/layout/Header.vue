74a55b24534c220e7a6227e311c750a9889f104a
puntodamar <punto.damar2@gmail.com>
2025-09-10 22:19:02 +0700

    fix: mobile close nav button

--- resources/js/components/layout/Header.vue
+++ resources/js/components/layout/Header.vue
@@ -36,8 +36,7 @@
         <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
             <div class="fixed inset-0 z-50" />
             <DialogPanel
-                class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-gray-800 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-            >
+                class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-gray-800 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
                     <a href="#" class="-m-1.5 p-1.5">
                         <span class="sr-only">Smash Sports</span>
@@ -45,7 +44,7 @@
                     </a>
                     <button type="button" class="-m-2.5 rounded-md p-2.5" :class="iconColor" @click="mobileMenuOpen = false">
                         <span class="sr-only">Close menu</span>
-                        <XMarkIcon class="size-6" aria-hidden="true" />
+                        <XMarkIcon class="size-6 text-black dark:text-white" aria-hidden="true" />
                     </button>
                 </div>
 
