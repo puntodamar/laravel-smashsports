42668fa582dfa75dd844a567cf97d7e05ef6bc24
puntodamar <punto.damar2@gmail.com>
2025-09-14 16:59:07 +0700

    fix: homepage routing

--- resources/js/components/layout/Header.vue
+++ resources/js/components/layout/Header.vue
@@ -2,10 +2,10 @@
     <header :class="[baseHeader, (isOverHero && props.dynamic) ? overHeroClasses : afterHeroClasses]">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
-                <a :href="route('home')" class="-m-1.5 p-1.5">
+                <Link :href="route('home')" class="-m-1.5 p-1.5">
                     <span class="sr-only">Smash Sports</span>
                     <img class="h-8 w-auto" :src="companyLogo" alt="" />
-                </a>
+                </Link>
             </div>
 
             <div class="flex items-center space-x-2 lg:hidden">
@@ -38,10 +38,10 @@
             <DialogPanel
                 class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-gray-800 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
-                    <a href="#" class="-m-1.5 p-1.5">
+                    <Link :href="route('home')" class="-m-1.5 p-1.5">
                         <span class="sr-only">Smash Sports</span>
                         <img class="h-8 w-auto" :src="companyLogo" alt="" />
-                    </a>
+                    </Link>
                     <button type="button" class="-m-2.5 rounded-md p-2.5" :class="iconColor" @click="mobileMenuOpen = false">
                         <span class="sr-only">Close menu</span>
                         <XMarkIcon class="size-6 text-black dark:text-white" aria-hidden="true" />
