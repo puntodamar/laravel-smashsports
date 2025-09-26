c1aef94107a060b21b217071e2e5640980264a44
puntodamar <punto.damar2@gmail.com>
2025-09-27 01:13:55 +0700

    fix: schedule on logged in user

--- Modules/Schedule/routes/web.php
+++ Modules/Schedule/routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Schedule\Http\Controllers\ScheduleController;
 
-Route::middleware('guest')->group(function () {
-    Route::prefix('schedule')->name('schedule.')->group(function () {
-        Route::get('/booking', [ScheduleController::class, 'booking'])->name('booking');
-    });
+Route::prefix('schedule')->name('schedule.')->group(function () {
+    Route::get('/booking', [ScheduleController::class, 'booking'])->name('booking');
 });
 
