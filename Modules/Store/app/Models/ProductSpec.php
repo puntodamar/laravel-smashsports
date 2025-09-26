ffff76978373e5d8c95a24cac01bc6d7b0c72dd5
puntodamar <punto.damar2@gmail.com>
2025-09-27 01:32:54 +0700

    add: product spec relationship

--- Modules/Store/app/Models/ProductSpec.php
+++ Modules/Store/app/Models/ProductSpec.php
@@ -4,6 +4,8 @@ namespace Modules\Store\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 // use Modules\Store\Database\Factories\ProductSpecFactory;
 
 class ProductSpec extends Model
@@ -14,6 +16,9 @@ class ProductSpec extends Model
      * The attributes that are mass assignable.
      */
     protected $fillable = ['product_id', 'label', 'description', 'sort_order'];
+    public function product(): BelongsTo {
+        return $this->belongsTo(Product::class);
+    }
 
     // protected static function newFactory(): ProductSpecFactory
     // {
